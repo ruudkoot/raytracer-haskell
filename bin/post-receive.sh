ead1dbc927d50d5cab90e40ce199aeec799dc7d1
Bart Spaans <bart.spaans@gmail.com>
2010-02-17 22:01:06 +0100

    Added documentation to build script (+ testing out build)

--- bin/post-receive.sh
+++ bin/post-receive.sh
@@ -1,12 +1,24 @@
 #!/bin/sh 
 
 
+# Update instance
 ./update_instance.sh && 
+
+# Update redmine clone 
 ./update_redmine_mirror.sh && 
+
+# Build programme
 cd .. && 
 cabal install && 
+
+# Run tests (configure with UserHooks)
 cabal test && 
+
+# Generate Haddock documentation
 cabal --executable haddock &&
+
+# Copy generated docs TODO
+
+# Run Hlint on all haskell source files
 find -name "*.hs" | xargs hlint
 
-# cp generated docs
