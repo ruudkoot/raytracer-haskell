5bed3a357c641e62502bc46cd8d218e847e51b37
Bart Spaans <bart.spaans@gmail.com>
2010-02-18 06:05:55 +0100

    Added more vector operations and documentation.

--- src/Shared/Vector.hs
+++ src/Shared/Vector.hs
@@ -4,24 +4,50 @@
 -- and functions for 2D, 3D and 4D vectors. 
 --
 module Shared.Vector where
+
+
   
 import Control.Applicative 
 
+
 -- * Vectors 
+
+
+-- | Vectors in its most general form can be used 
+-- to fold and zip with. When the instance of Vector 
+-- is carrying Numerical values, they can also 
+-- be used as built-in numerical values. 
+-- For example: @2 * Vector3D (1,2,3)@
+--
+class (Applicative v) => Vector v where 
+  fromVector :: v a -> [a]
+  zipWithVectors :: (a -> b -> c) -> v a -> v b -> v c
+  foldVector :: ([a] -> b) -> v a -> b
+  zipWithVectors f v1 v2 = f <$> v1 <*> v2
+  foldVector f = f . fromVector 
+
+instance Vector Vector2D where
+  fromVector (Vector2D (x, y)) = [x, y]
+
+instance Vector Vector3D where
+  fromVector (Vector3D (x, y, z)) = [x, y, z]
+
+instance Vector Vector4D where 
+  fromVector (Vector4D (x, y, z, a)) = [x, y, z, a]
+
+
+
+-- | A general two-dimensional vector.
 --
 newtype Vector2D a = Vector2D (a, a) deriving (Show, Eq)
+
+-- | A general three-dimensional vector.
+--
 newtype Vector3D a = Vector3D (a, a, a) deriving (Show, Eq)
+
+-- | A general four-dimensional vector.
+--
 newtype Vector4D a = Vector4D (a, a, a, a) deriving (Show, Eq)
-
-
-fromVector2D :: Vector2D a -> (a, a)
-fromVector2D (Vector2D a) = a
-
-fromVector3D :: Vector3D a -> (a, a, a)
-fromVector3D (Vector3D a) = a
-
-fromVector4D :: Vector4D a -> (a, a, a, a)
-fromVector4D (Vector4D a) = a
 
 
 instance Functor Vector2D where 
@@ -49,22 +75,16 @@ instance Applicative Vector4D where
 
 
 
+-- * Out of the Vector* context
+--
+fromVector2D :: Vector2D a -> (a, a)
+fromVector2D (Vector2D a) = a
 
-class (Applicative v) => Vector v where 
-  fromVector :: v a -> [a]
-  zipWithVectors :: (a -> b -> c) -> v a -> v b -> v c
-  foldVector :: ([a] -> b) -> v a -> b
-  zipWithVectors f v1 v2 = f <$> v1 <*> v2
-  foldVector f = f . fromVector 
+fromVector3D :: Vector3D a -> (a, a, a)
+fromVector3D (Vector3D a) = a
 
-instance Vector Vector2D where
-  fromVector (Vector2D (x, y)) = [x, y]
-
-instance Vector Vector3D where
-  fromVector (Vector3D (x, y, z)) = [x, y, z]
-
-instance Vector Vector4D where 
-  fromVector (Vector4D (x, y, z, a)) = [x, y, z, a]
+fromVector4D :: Vector4D a -> (a, a, a, a)
+fromVector4D (Vector4D a) = a
 
 
 
@@ -114,21 +134,35 @@ instance (Num a, Vector v, Show (v a), Eq (v a)) => Num (v a) where
   fromInteger = pure . fromInteger
 
 
--- The class constraints in the following definitions are also pretty awful.
+
+-- * Vector Operations
+-- The class constraints in the following definitions are also pretty awful 
+-- (see the source for more details if you are reading this comment from haddock)
 
 
 -- | The dot product is an operation that takes two equal-length Vectors 
 -- of numbers  and returns a single number obtained by multiplying 
 -- corresponding entries and adding up those products. 
 --
-dotProduct :: (Vector v, Num a, Num (v a)) => v a -> v a -> a
-dotProduct v1 v2 = foldVector sum (v1 * v2)
+dot :: (Vector v, Num a, Num (v a)) => v a -> v a -> a
+dot v1 v2 = foldVector sum (v1 * v2)
+
+
+-- | The cross product is a binary operation on two 3D vectors 
+-- that results in another vector which is perpendicular to 
+-- the plane containing the two input vectors.
+--
+crossProduct :: Num a => Vector3D a -> Vector3D a -> Vector3D a
+crossProduct (Vector3D (x1, y1, z1)) (Vector3D (x2, y2, z2)) = 
+  Vector3D ((y1 * z2 - z1 * y2),
+            (z1 * x2 - x1 * z2),
+            (x1 * y2 - y1 * z2))
 
 
 -- | Calculates the length or magnitude of the given Vector.
 --
 magnitude :: (Vector v, Floating a, Num (v a)) => v a -> a
-magnitude v = sqrt (dotProduct v v)
+magnitude v = sqrt (v `dot` v)
 
 
 -- | A version of magnitude for Integrals. 
@@ -138,8 +172,8 @@ magnitude' :: (Vector v, Integral a, Num (v a), Num (v Double)) => v a -> Double
 magnitude' = magnitude . fmap fromIntegral
 
 
+-- | Normalize the length of the vector to @1.0@.
+--
+normalize :: (Vector v, Floating a, Num (v a)) => v a -> v a 
+normalize v1 = pure (1 / magnitude v1) * v1
 
--- Missing (at least):
---   normalize :: v a -> v a
---   crossProduct :: Vector3D a -> Vector3D a
---   
