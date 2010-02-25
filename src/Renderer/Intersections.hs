3370a7205ecda9ca7f9c00ed8a8885c10a5713f2
Bart Spaans <bart.spaans@gmail.com>
2010-02-26 00:41:01 +0100

    Intersections stubs geupdate, zodat ze primitieven gebruiken

--- src/Renderer/Intersections.hs
+++ src/Renderer/Intersections.hs
@@ -3,3 +3,19 @@ module Renderer.Intersections where
 import Renderer.Datatypes
 import Renderer.Primitives
 
+type Intersection = (Point3D, Vector3D) -- At point x, in direction y
+
+hit :: Ray -> Shape -> Bool
+hit r Cube     = undefined
+hit r Cylinder = undefined
+hit r Sphere   = undefined
+hit r Cone     = undefined
+hit r Plane    = undefined
+
+intersection :: Ray -> Shape -> [Intersection]
+intersection r Cube     = undefined
+intersection r Cylinder = undefined
+intersection r Sphere   = undefined
+intersection r Cone     = undefined
+intersection r Plane    = undefined
+
