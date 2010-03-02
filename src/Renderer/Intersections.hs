2ffcb13ffcebb9d503296677be77f034103e4afe
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-02 14:04:19 +0100

    Changed Intersection Datatype, fixed Sphere hit&intersect.

--- src/Renderer/Intersections.hs
+++ src/Renderer/Intersections.hs
@@ -4,14 +4,25 @@ import Shared.Vector
 import Renderer.Datatypes
 import Data.Ord
 
-type Intersection = (Pt3D, Vec3D) -- At point x, in direction y
---isn't direction already defined in the ray? or is this the normal direction?
---Should return distance in t? instead of point?
+type Intersection = (Double, Double) -- Enters at x, leaves at y
 
 hit :: Ray -> Shape -> Bool
 hit r Cube     = undefined
 hit r Cylinder = undefined
-hit r Sphere   = undefined
+hit r Sphere   = let dir = rDirection r
+                     --constant throughout rendering, optimization?
+                     k = rOrigin r
+                     --optimization: can be assumed 1 when dir is a unit vector
+                     a = dir <.> dir
+                     b = 2.0 * (k <.> dir)
+                     --also constant because k is constant
+                     c = (k <.> k) - 1.0 
+                     --4*a*c can be constant, see above
+                     d = b*b - 4.0*a*c
+                  in case compare d 0.0 of
+                        LT -> False
+                        _  -> True
+                        
 hit r Cone     = undefined
 hit r Plane    = let vd = sumVector $ rDirection r
                      v0 = sumVector $ rOrigin r
@@ -25,8 +36,7 @@ intersection r Cylinder = undefined
 
 --Formula from http://www.devmaster.net/wiki/Ray-sphere_intersection, took out the k = (o-c) constant with c = (0,0,0).
 
-intersection r Sphere   = undefined{-
-                            let dir = rDirection r
+intersection r Sphere   = let dir = rDirection r
                               --constant throughout rendering, optimization?
                               k = rOrigin r 
                               --optimization: can be assumed 1 when dir is a unit vector
@@ -37,14 +47,10 @@ intersection r Sphere   = undefined{-
                               --4*a*c can be constant, see above
                               d = b*b - 4.0*a*c
                           in case compare d 0.0 of
-                                EQ -> [(-b/(2*a), Vector3D (0,0,0))]
+                                EQ -> [(-b/(2*a),-b/(2*a))]
                                 LT -> []
                                 _  -> let sqrd = sqrt d
-                                      in [((-b-sqrd)/(2*a), Vector3D (0,0,0))
-                                         ,((-b+sqrd)/(2*a), Vector3D (0,0,0))]
-
--}
+                                      in [((-b-sqrd)/(2*a), (-b+sqrd)/(2*a))]
 
 intersection r Cone     = undefined
 intersection r Plane    = undefined
-
