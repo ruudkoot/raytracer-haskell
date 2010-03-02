ed9f6202c21701da27eea98b786c52e90a21c8f6
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-02 14:26:36 +0100

    These optimalisations are not possible: the transformations on the ray don't preserve the length or origin.

--- src/Renderer/Intersections.hs
+++ src/Renderer/Intersections.hs
@@ -10,14 +10,10 @@ hit :: Ray -> Shape -> Bool
 hit r Cube     = undefined
 hit r Cylinder = undefined
 hit r Sphere   = let dir = rDirection r
-                     --constant throughout rendering, optimization?
                      k = rOrigin r
-                     --optimization: can be assumed 1 when dir is a unit vector
                      a = dir <.> dir
                      b = 2.0 * (k <.> dir)
-                     --also constant because k is constant
-                     c = (k <.> k) - 1.0 
-                     --4*a*c can be constant, see above
+                     c = (k <.> k) - 1.0
                      d = b*b - 4.0*a*c
                   in case compare d 0.0 of
                         LT -> False
@@ -39,14 +35,10 @@ intersection r Cylinder = undefined
 --Formula from http://www.devmaster.net/wiki/Ray-sphere_intersection, took out the k = (o-c) constant with c = (0,0,0).
 
 intersection r Sphere   = let dir = rDirection r
-                              --constant throughout rendering, optimization?
                               k = rOrigin r 
-                              --optimization: can be assumed 1 when dir is a unit vector
                               a = dir <.> dir 
                               b = 2.0 * (k <.> dir)
-                              --also constant because k is constant
-                              c = (k <.> k) - 1.0 
-                              --4*a*c can be constant, see above
+                              c = (k <.> k) - 1.0
                               d = b*b - 4.0*a*c
                           in case compare d 0.0 of
                                 EQ -> [(-b/(2*a),-b/(2*a))]
