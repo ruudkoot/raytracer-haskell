3d6ce3b53bf496c651dcac1736d36661f77f3d27
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-05 15:41:23 +0100

    And now within 80 characters.

--- src/Renderer/Renderer.hs
+++ src/Renderer/Renderer.hs
@@ -9,14 +9,16 @@ import Renderer.Datatypes
 import Renderer.Intersections
 
 renderScene :: ObjectTree a -> Width -> Height -> Double -> Colours Int
-renderScene (Renderer.Datatypes.RSimple o m) w h fov = [if Renderer.Intersections.hit (ray i j) o
-                                                        then Output.Output.Colour (255,255,255)
-                                                        else Output.Output.Colour (0,0,0)
-                                                        | i <- [0..(fromSize w)-1], j <- [0..(fromSize h)-1]]
-  where ray i j = Renderer.Datatypes.Ray (minv !*! Shared.Vector.Vector3D (0,0,-1))
-                                        (minv !*! Shared.Vector.Vector3D (x - ((fromInteger j) + 0.5) * delta,
-                                        y - ((fromInteger i) + 0.5) * delta, 1))
+renderScene (RSimple o m) w h fov = [if hit (ray i j) o
+                                     then Colour (255,255,255)
+                                     else Colour (0,0,0)
+                                     | i <- [0..fromSize w-1],
+                                       j <- [0..fromSize h-1]]
+  where ray i j = Ray (minv !*! Vector3D (0,0,-1))
+                      (minv !*! Vector3D (x - (fromInteger j + 0.5) * delta,
+                                        y - (fromInteger i + 0.5) * delta, 1))
         x = tan(0.5 * fov)
-        y = tan(0.5 * fov) * ((fromInteger $ fromSize h) / (fromInteger $ fromSize w))
-        delta = 2 * tan(0.5 * fov) / (fromInteger $ fromSize w)
+        y = tan(0.5 * fov) * (fromInteger (fromSize h)
+                           /  fromInteger (fromSize w))
+        delta = 2 * tan(0.5 * fov) / fromInteger (fromSize w)
         minv = inverse m
