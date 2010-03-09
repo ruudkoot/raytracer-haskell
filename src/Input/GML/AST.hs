feaaae7ceb9dba3345921b4b4c3e5e459bbd323a
Ruud Koot <git@ruudkoot.nl>
2010-03-09 16:45:53 +0100

    fix very fatal type error

--- src/Input/GML/AST.hs
+++ src/Input/GML/AST.hs
@@ -109,7 +109,7 @@ simplePrintAlg = ((func,arr,op,ident,bind,base),(int,real,string,bool))
 
 
 simplePrintGML::Code -> String
-simplePrintGML = concatMap (' ':) $ foldCode simplePrintAlg
+simplePrintGML = concatMap (' ':) . foldCode simplePrintAlg
 
 --Arbitrary instances for GML        
 
