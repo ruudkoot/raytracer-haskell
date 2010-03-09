7bf38ccc90471aebf7093540da869584aad826a1
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-09 15:39:52 +0100

    Fixed a small bug with non-square images.

--- src/Input/GML/ToRenderObject.hs
+++ src/Input/GML/ToRenderObject.hs
@@ -2,16 +2,16 @@ module Input.GML.ToRenderObject where
 
 import           Data.Matrix
 import           Data.Vector
-
-import qualified Input.GML.Scene    as Scene
+import           Base.Shader
+import qualified Input.GML.Scene   as Scene
 import qualified Base.Miscellaneous as Render
 
-toRenderObject :: Scene.Object -> Render.ObjectTree Render.Shader
+toRenderObject :: Scene.Object -> Render.ObjectTree Shader
 toRenderObject = flip (Scene.foldObject algebra) identity4D
     where algebra = ( \shape shader matrix -> Render.RSimple shape matrix (inverse matrix) shader
                     , \o d1 d2 d3 matrix -> o ((translate d1 d2 d3) !*! matrix)
                     , \o d1 d2 d3 matrix -> o ((diagonal4D (Vector4D (d1, d2, d3, 1))) !*! matrix)
-                    , \o d matrix -> o ((scaleF d identity4D) !*! matrix)
+                    , \o d matrix -> o ((diagonal4D (Vector4D (d, d, d, 1))) !*! matrix)
                     , \o d matrix -> o ((rotateX d) !*! matrix)
                     , \o d matrix -> o ((rotateY d) !*! matrix)
                     , \o d matrix -> o ((rotateZ d) !*! matrix)
