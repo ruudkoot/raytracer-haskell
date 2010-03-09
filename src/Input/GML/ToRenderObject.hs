c0f6dda6804180ab3cb8dfea8256de1b48bff1cb
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-09 14:58:02 +0100

    Oh, and a better name for it. ^^

---
+++ src/Input/GML/ToRenderObject.hs
@@ -0,0 +1,36 @@
+module Input.GML.ToRenderObject where
+
+import           Shared.Matrix
+import           Shared.Vector
+import qualified Input.GML.Scene   as Scene
+import qualified Shared.RenderBase as Render
+
+toRenderObject :: Scene.Object -> Render.ObjectTree Render.Shader
+toRenderObject = flip (Scene.foldObject algebra) identity4D
+    where algebra = ( \shape shader matrix -> Render.RSimple shape matrix (inverse matrix) shader
+                    , \o d1 d2 d3 matrix -> o ((translate d1 d2 d3) !*! matrix)
+                    , \o d1 d2 d3 matrix -> o ((diagonal4D (Vector4D (d1, d2, d3, 1))) !*! matrix)
+                    , \o d matrix -> o ((scaleF d identity4D) !*! matrix)
+                    , \o d matrix -> o ((rotatex d) !*! matrix)
+                    , \o d matrix -> o ((rotatey d) !*! matrix)
+                    , \o d matrix -> o ((rotatez d) !*! matrix)
+                    , \o1 o2 matrix -> Render.RUnion      (o1 matrix) (o2 matrix)
+                    , \o1 o2 matrix -> Render.RIntersect  (o1 matrix) (o2 matrix)
+                    , \o1 o2 matrix -> Render.RDifference (o1 matrix) (o2 matrix)
+                    )
+          translate a b c = Matrix4D( Vector4D(1, 0, 0, a)
+                                    , Vector4D(0, 1, 0, b)
+                                    , Vector4D(0, 0, 1, c)
+                                    , Vector4D(0, 0, 0, 1))
+          rotatex d = Matrix4D( Vector4D(1, 0, 0, 0)
+                              , Vector4D(0, cos d, -sin d, 0)
+                              , Vector4D(0, sin d, cos d, 0)
+                              , Vector4D(0, 0, 0, 1))
+          rotatey d = Matrix4D( Vector4D(cos d, 0, -sin d, 0)
+                              , Vector4D(0, 1, 0, 0)
+                              , Vector4D(sin d, 0, cos d, 0)
+                              , Vector4D(0, 0, 0, 1))
+          rotatez d = Matrix4D( Vector4D(cos d, -sin d, 0, 0)
+                              , Vector4D(sin d, cos d, 0, 0)
+                              , Vector4D(0, 0, 1, 0)
+                              , Vector4D(0, 0, 0, 1))
