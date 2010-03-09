ce747e70e2ed3471cd29718ba6765caf61525728
Ruud Koot <git@ruudkoot.nl>
2010-03-09 15:10:57 +0100

    refactor shizzle

---
+++ src/Base/Shader.hs
@@ -0,0 +1,20 @@
+module Base.Shader where
+
+import Shared.Colour
+
+-- ** Shaders
+type ShaderCoord = (Double,Double,Int)
+data ShaderResult = ShaderResult
+    {
+     srColor::ColourD
+    ,srKd::Double
+    ,srKs::Double
+    ,srPhong::Double
+    }
+
+newtype Shader = Shader (ShaderCoord -> ShaderResult)
+instance Show Shader where
+    show s = "Shader function"
+instance Eq Shader where
+    (==) a b = error "Alessandro" --True
+
