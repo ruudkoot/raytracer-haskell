06815587b6858034c4a70eda175871801e2ca5c8
Ruud Koot <git@ruudkoot.nl>
2010-03-09 15:07:46 +0100

    refactor shizzle

--- src/Input/GML/Scene.hs
+++ src/Input/GML/Scene.hs
@@ -2,9 +2,9 @@ module Input.GML.Scene where
   
 import Shared.Vector
 
-import qualified Shared.RenderBase as Base
-import qualified Base.Light as Light
-import qualified Base.Shape as Shape
+import qualified Base.Light  as Light
+import qualified Base.Shader as Shader
+import qualified Base.Shape  as Shape
 
 data Scene = Scene
   { sceneAmbience :: Pt3D
@@ -18,7 +18,7 @@ data Scene = Scene
   }
   deriving (Show,Eq)
 
-data Object = Simple     Shape.Shape  Base.Shader
+data Object = Simple     Shape.Shape  Shader.Shader
             | Translate  Object Double Double Double
             | Scale      Object Double Double Double
             | UScale     Object Double 
@@ -31,7 +31,7 @@ data Object = Simple     Shape.Shape  Base.Shader
             deriving (Show,Eq)
           
 type ObjectAlgebra r =
-    ( Shape.Shape -> Base.Shader -> r
+    ( Shape.Shape -> Shader.Shader -> r
     , r -> Double -> Double -> Double -> r
     , r -> Double -> Double -> Double -> r
     , r -> Double -> r
