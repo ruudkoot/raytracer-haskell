9d546ff47a0cbb50d034c6235eb7b45df7a6ae3a
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-09 14:14:33 +0100

    Added a matrix as the environment of the tree-fold.

--- src/Input/GML/ToThijsObject.hs
+++ src/Input/GML/ToThijsObject.hs
@@ -5,16 +5,16 @@ import qualified Input.GML.Render  as Render
 import qualified Shared.RenderBase as Thijs
 
 toThijsObject :: Render.Object -> Thijs.ObjectTree Thijs.Shader
-toThijsObject = Render.foldObject algebra
-    where algebra = ( \shape shader -> Thijs.RSimple shape identity4D identity4D shader
+toThijsObject = flip (Render.foldObject algebra) identity4D
+    where algebra = ( \shape shader matrix -> Thijs.RSimple shape matrix matrix shader
                     , undefined
                     , undefined
                     , undefined
                     , undefined
                     , undefined
                     , undefined
-                    , \o1 o2 -> Thijs.RUnion      o1 o2
-                    , \o1 o2 -> Thijs.RIntersect  o1 o2
-                    , \o1 o2 -> Thijs.RDifference o1 o2
+                    , \o1 o2 matrix -> Thijs.RUnion      (o1 matrix) (o2 matrix)
+                    , \o1 o2 matrix -> Thijs.RIntersect  (o1 matrix) (o2 matrix)
+                    , \o1 o2 matrix -> Thijs.RDifference (o1 matrix) (o2 matrix)
                     )
 
