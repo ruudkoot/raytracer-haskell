fe26e652bd5d2bfac2199860631397848b1e37c1
Bram Schuur <bramschuur@hotmail.com>
2010-03-09 16:12:09 +0100

    Updating project to run entire chain

--- src/Project.hs
+++ src/Project.hs
@@ -5,6 +5,8 @@ import Control.Applicative ((<$>))
 import Input.GML.AST
 import Input.GML.Parser
 import Input.GML.Evaluate
+import Input.GML.ToRenderObject
+import Input.GML.Scene
 --
 import Renderer.Datatypes
 import Renderer.Intersections
@@ -18,14 +20,19 @@ import System.IO
 
 import Data.Map as M
 
-test::String
-test ="{ /a /b /c b c } /solveRoot 3.0 4.0 5.0 solveRoot apply"
+doParseGML::IO GML
+doParseGML = do parseResult <- parseGML <$> getContents
+                case parseResult of
+                  Left err -> error $ "Parse error on " ++ show err
+                  Right (tks) -> return tks
 
+doEvaluateGML::GML->Scene
+doEvaluateGML gml = case evaluate (M.empty,[],gml) of
+                        (_,(Render s:_),_) -> s
+                        _            -> error "No render object found"
+           
 main :: IO()
-main = do parseResult <- parseGML <$> getContents
-          case parseResult of
-            Left err -> putStrLn $ "Parse error on " ++ show err
-            Right (tks) -> print (evaluate (M.empty,[],tks))
+main = do gml <- doParseGML          
           maybe (putStrLn "Not a valid image") putStrLn (toPPM (Size 0) (Size 0) [])
 
 -- main = Renderer.Renderer.renderTest
