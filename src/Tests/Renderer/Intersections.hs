3fab00bf6e28c7f1a5bd435d44f9a67c4a26eb92
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-18 16:17:16 +0100

    Updated quickCheck test for the intervals.

--- src/Tests/Renderer/Intersections.hs
+++ src/Tests/Renderer/Intersections.hs
@@ -1,10 +1,31 @@
 module Tests.Renderer.Intersections where
 
--- test (Ray o d) = map (\(x,y) -> (x, magnitude $ Vector4DD (1, 0, 1, 0) * (o + scaleF x d), y, magnitude $ Vector4DD (1, 0, 1, 0) * (o + scaleF y d))) (intersection (Ray o d) Cylinder)
-
-import Renderer.Intersections
+import Test.QuickCheck
+import Renderer.Intersections2
+import Renderer.Scene
 import Data.Vector
+import Data.Matrix
+import Base.Shape
+import Control.Monad
+import Debug.Trace
+
+-- test (Ray o d) = map (\(x,y) -> (x, magnitude $ Vector4D (1, 0, 1, 0) * (o + scaleF x d), y, magnitude $ Vector4D (1, 0, 1, 0) * (o + scaleF y d))) (intersection (Ray o d) Cylinder)
+
+margin :: Double
+margin = 0.001
+
+instance Arbitrary Ray where
+  arbitrary = liftM (Ray (Vector4D (0, 0, -4, 0))) arbitrary
 
 prop_cylinderIntersect :: Ray -> Bool
-prop_cylinderIntersect (Ray o d) = map (\(x,y) -> isOnCylinder (o + scaleF x d) && isOnCylinder (o + scaleF y d)) (intersection r Cylinder)
-   where isOnCylinder v = abs ((getY4D v) - 1.0) < 0.001 || abs ((sqrt ((getX4D v)*(getX4D v) + (getZ4D v)*(getZ4D v))) - 1.0) < 0.001
+prop_cylinderIntersect (Ray o d) = magnitudeSquared d == 0 || all (\(x,y) -> isOnCylinder (mkPoint x) && isOnCylinder (mkPoint y)) (intervals (Ray o d) Cylinder)
+   where isOnCylinder v = isOnTop v || isOnBottom v || isOnSide v
+         isOnTop v      = abs (getY4D v) - 1.0 < margin
+         isOnBottom v   = abs (getY4D v) < margin
+         isOnSide v     = abs (sqrt ((getX4D v)*(getX4D v) + (getZ4D v)*(getZ4D v)) - 1.0) < margin
+         mkPoint t      = o + scaleF t d
+         
+prop_sphereIntersect :: Ray -> Bool
+prop_sphereIntersect (Ray o d) = magnitudeSquared d == 0 || all (\(x,y) -> isOnSphere (mkPoint x) && isOnSphere (mkPoint y)) (intervals (Ray o d) Sphere)
+  where isOnSphere v   = abs ((magnitudeSquared $ dropW v) - 1.0) < margin
+        mkPoint t      = o + scaleF t d
