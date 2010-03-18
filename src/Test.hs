bd4786f2e391a50ab39690e2e2ee9037bb6dc404
Thijs Alkemade <thijsalkemade@gmail.com>
2010-03-18 16:20:28 +0100

    And add them so they run automatically.

--- src/Test.hs
+++ src/Test.hs
@@ -1,8 +1,11 @@
 module Test where
 import Tests.Input.GML.Parser
+import Tests.Renderer.Intersections
 import Test.QuickCheck
 
-allTests = [prop_parseOK
+allTests = [ prop_parseOK
+           , prop_cylinderIntersect
+           , prop_sphereIntersect
            ]
 
 runTests::IO()
