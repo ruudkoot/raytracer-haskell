87502bc47420f310d66915908830c6265920c827
Bart Spaans <bart.spaans@gmail.com>
2010-03-23 05:49:32 +0100

    Calculating the right intensity of spotlights and pointlights.

--- src/Renderer/Lighting.hs
+++ src/Renderer/Lighting.hs
@@ -18,19 +18,31 @@ import Control.Applicative ((<$>))
 localLighting :: ColourD -> IntersectionInfo -> [RenderLight] -> SurfaceProperty -> Ray -> Vec3D -> Vec3D
 localLighting (Colour amb) its lights surface r reflected = kdiac + kdsum + kssum + ksisc
   where kdiac = (kd *) <$> amb * c
-        kdsum = (kd *) <$> sum (map (\l -> ((n !.! direction l) *) <$> getIntensity l * c) lights)
+        kdsum = (kd *) <$> sum (map (\l -> ((n !.! direction l) *) <$> intensity l * c) lights)
         ksisc = (ks *) <$> reflected * c
-        kssum = (ks *) <$> sum (map (\l -> ((n !.! dirhalf l) ** phongExponent surface *) <$> getIntensity l * c) lights)
+        kssum = (ks *) <$> sum (map (\l -> ((n !.! dirhalf l) ** phongExponent surface *) <$> intensity l * c) lights)
         kd = diffuseReflectionCoefficient surface
         ks = specularReflectionCoefficient surface
         n = normal its
         (Colour c) = surfaceColour surface
-        getIntensity (DirectLight _ i) = i 
-        getIntensity (PointLight  _ i) = i
-        getIntensity (SpotLight _ _ i _ _) = i
+        intensity l = getIntensity l (location its) 
         direction (PointLight pos _) = normalize $ pos - location its
         direction (DirectLight dir _) = normalize $ negate dir
         direction (SpotLight pos _ _ _ _) = normalize $ pos - location its
         dirhalf l = normalize (dropW (rDirection r) `cross` direction l) -- not sure about this
         eye = toVec3D 0 0 (-1)
-                
+ 
+getIntensity :: RenderLight -> Pt3D -> Vec3D               
+getIntensity (DirectLight _  i) _ = i 
+getIntensity (PointLight pos i) loc = attenuate i (magnitude $ abs (loc - pos))
+getIntensity (SpotLight pos at i cutoff exp) loc = attenuate i' (magnitude $ abs (loc - pos))
+  where i' = if angle > cutoff then toVec3D 0 0 0 else spot 
+        spot = (((dir / (abs dir)) !.! (posDir / (abs posDir))) ** exp *) <$> i
+        dir = at - pos 
+        posDir = loc - pos
+        angle = acos(dir !.! posDir)
+
+
+attenuate :: Vec3D -> Double -> Vec3D
+attenuate i d = fmap (\x -> 100 * x / (99 + d ** 2)) i 
+
