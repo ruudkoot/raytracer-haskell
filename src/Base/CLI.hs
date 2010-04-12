a6a35f883563fa3d86f12eb33f5b891ce6eafacb
Alessandro Vermeulen <avermeulen@spockz.nl>
2010-04-12 10:25:42 +0200

    Rename + added file :)

--- src/Base/CLI.hs
+++ src/Base/CLI.hs
@@ -3,21 +3,23 @@ module Base.CLI where
 data CLArgs = CLArgs 
   { aa    :: Int
   , bloom :: Bool
+  , file  :: String
   } deriving Show
   
-defaultargs = CLArgs { aa = 1,bloom = False }
+defaultargs = CLArgs { aa = 1,bloom = False, file = "" }
 
 -- | This looks an awful lot like what we used to do in T&C
-parseVargs :: [(String, (CLArgs, [String]) -> (CLArgs, [String]) )] -> CLArgs -> [String]  -> CLArgs
-parseVargs funcs def input = p (def, input)
+parseCargs :: [(String, (CLArgs, [String]) -> (CLArgs, [String]) )] -> CLArgs -> [String]  -> CLArgs
+parseCargs funcs def input = p (def, input)
   where p (res , [])                = res
         p (res , ('-':flag) : xs) = p (sLookup flag res xs)
+        p (res , xs)              = res { file = concat xs }
         sLookup key def inp = maybe (def, inp) 
                                     (\f -> f (def, inp)) 
                                     (lookup key funcs)
 
 -- | Example usage of parseVargs.
-ex = parseVargs [
+ex = parseCargs [
                  ("bloom", \(args, inp)  -> (args { bloom = True  }, inp))
                 ,("aa"   , \(args, i:xs) -> (args { aa    = read i}, xs ))
                 ]
