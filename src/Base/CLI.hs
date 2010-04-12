77926a9adb2c40e5233e29c419a1edbe38027777
Alessandro Vermeulen <avermeulen@spockz.nl>
2010-04-12 09:48:49 +0200

    hlint. :)

--- src/Base/CLI.hs
+++ src/Base/CLI.hs
@@ -8,17 +8,17 @@ data CLArgs = CLArgs
 defaultargs = CLArgs { aa = 1,bloom = False }
 
 -- | This looks an awful lot like what we used to do in T&C
-parseVargs :: [(String, ( (CLArgs, [String]) -> (CLArgs, [String]) ))] -> CLArgs -> [String]  -> CLArgs
+parseVargs :: [(String, (CLArgs, [String]) -> (CLArgs, [String]) )] -> CLArgs -> [String]  -> CLArgs
 parseVargs funcs def input = p (def, input)
   where p (res , [])                = res
-        p (res , (('-':flag) : xs)) = p (sLookup flag res xs)
+        p (res , ('-':flag) : xs) = p (sLookup flag res xs)
         sLookup key def inp = maybe (def, inp) 
                                     (\f -> f (def, inp)) 
                                     (lookup key funcs)
 
 -- | Example usage of parseVargs.
 ex = parseVargs [
-                 ("bloom", \(args, inp)    -> (args { bloom = True } , inp))
-                ,("aa"   , \(args, (i:xs)) -> (args { aa    = read i}, xs ))
+                 ("bloom", \(args, inp)  -> (args { bloom = True  }, inp))
+                ,("aa"   , \(args, i:xs) -> (args { aa    = read i}, xs ))
                 ]
                 defaultargs
