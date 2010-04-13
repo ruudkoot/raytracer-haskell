9657d2253796bd2242ccb6ac7423bd2c70ab23b4
Bart Spaans <bart.spaans@gmail.com>
2010-04-13 12:24:23 +0200

    Moved Output.hs into PPM.hs

--- src/Output/Output.hs
+++ src/Output/Output.hs
@@ -4,29 +4,3 @@ module Output.Output ( Size(..), fromSize, toSize,
                      ) where
 
 
-import Data.Colour
-
-
--- | Size is a positive integer (i.e. a natural number)
---
-newtype Size = Size Integer deriving Eq
-
-instance Show Size where 
-  show (Size i) = show i
-
-
-fromSize :: Size -> Integer
-fromSize (Size s) = s
-
-toSize :: Int -> Size 
-toSize = Size . fromIntegral
-
-type Width = Size
-type Height = Size
-
-
--- | The type of the image converters. Expects the colours in 
--- Colours Int to be in the [0,255] range.
---
-type ImageWriter = Width -> Height -> Colours Int -> Maybe String
-
