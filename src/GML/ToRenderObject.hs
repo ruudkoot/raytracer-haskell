d7a59b41e60b69123812c75a413946fcf9c9f882
Bart Spaans <bart.spaans@gmail.com>
2010-04-13 21:00:27 +0200

    Some fixes in the bounding boxes. References going wrong: csg, plane, intercubes, cone and golf

--- src/GML/ToRenderObject.hs
+++ src/GML/ToRenderObject.hs
@@ -6,7 +6,7 @@ import           Data.Colour
 import           Data.Transformation
 import           Data.Texture
 import           Data.Vector
-import           Data.Glome.Vec (Bbox(..), bbjoin, bboverlap, xfm_vec, Vec(..)) 
+import           Data.Glome.Vec hiding (translate, scale)
 
 import qualified GML.AST        as GML
 import qualified GML.Evaluate   as Evil
@@ -22,22 +22,22 @@ toRenderObject txs = flip (GML.foldObject algebra) identityTransformation
             , \o d           trans -> o (trans !*! rotateX (toRadians d))
             , \o d           trans -> o (trans !*! rotateY (toRadians d))
             , \o d           trans -> o (trans !*! rotateZ (toRadians d))
-            , \o1 o2         trans -> Renderer.Union      (o1 trans) (o2 trans) (bbjoin (bbox (o1 trans) trans) (bbox (o2 trans) trans))
-            , \o1 o2         trans -> Renderer.Intersect  (o1 trans) (o2 trans) (bbjoin (bbox (o1 trans) trans) (bbox (o2 trans) trans))
-            , \o1 o2         trans -> Renderer.Difference (o1 trans) (o2 trans) (bbjoin (bbox (o1 trans) trans) (bbox (o2 trans) trans))
+            , \o1 o2         trans -> Renderer.Union      (o1 trans) (o2 trans) (bbjoin (bbox $ o1 trans) (bbox $ o2 trans) `transformBbox` trans)
+            , \o1 o2         trans -> Renderer.Intersect  (o1 trans) (o2 trans) (bboverlap (bbox $ o1 trans) (bbox $ o2 trans) `transformBbox` trans)
+            , \o1 o2         trans -> Renderer.Difference (o1 trans) (o2 trans) (bbjoin (bbox $ o1 trans) (bbox $ o2 trans) `transformBbox` trans)
             )
                     
-bbox :: Renderer.Object -> Transformation -> Bbox 
-bbox (Renderer.Simple s   _ _ b) = transformBbox b
-bbox (Renderer.Union      _ _ b) = transformBbox b 
-bbox (Renderer.Intersect  _ _ b) = transformBbox b
-bbox (Renderer.Difference _ _ b) = transformBbox b
+bbox :: Renderer.Object -> Bbox 
+bbox (Renderer.Simple s   _ _ b) = b
+bbox (Renderer.Union      _ _ b) = b 
+bbox (Renderer.Intersect  _ _ b) = b
+bbox (Renderer.Difference _ _ b) = b
 
 transformBbox :: Bbox -> Transformation -> Bbox 
 transformBbox (Bbox p1 p2) trans = Bbox (toVec3D (min x1 x2) (min y1 y2) (min z1 z2))
                                         (toVec3D (max x1 x2) (max y1 y2) (max z1 z2))
-  where (Vec x1 y1 z1) = transformPoint trans p1 
-        (Vec x2 y2 z2) = transformPoint trans p2
+  where (Vec x1 y1 z1) = invxfm_vec trans p1 
+        (Vec x2 y2 z2) = invxfm_vec trans p2
 
 toWorld :: Textures -> GML.Scene -> Renderer.Scene
 toWorld txs (GML.Scene amb l obj dp fov w h fil) = 
